852dfcdaae5c407810da2de78c0ccfe67ae03505
Misako Kondoh <misapipipink@gmail.com>
2021-03-27 13:59:18 +0900

    create server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const port = 5000;
+app.listen(port, () => {
+    console.log(`Server has connected on port ${port}✨`);
+});
