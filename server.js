f91fcffe84376fe671332f5edddb7e4d420a0eed
Misako Kondoh <misapipipink@gmail.com>
2021-03-27 17:21:52 +0900

    set up client, server, proxy

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+const knex = require('./db/knex');
 
 const app = express();
 
@@ -8,7 +10,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// app.use(express.static(__dirname + '/build'));
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server has connected to port ${port}✨`);
 });
+
+//Always return the main index.html, since we are developing a single page application
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/build/index.html"));
+  });
