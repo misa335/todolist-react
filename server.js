ed5545ee6a992c6229cdbcfd5665b7086cd6e9ee
Misako Kondoh <misapipipink@gmail.com>
2021-03-28 18:32:49 +0900

    fixed server.js 2

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/build'));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
